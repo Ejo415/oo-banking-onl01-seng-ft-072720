83e8e21f55ed61679ad7e70b539cd7be8520ed5b
Eugene O'Neil <ejo7220@gmail.com>
2020-08-11 16:51:02 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 class BankAccount
 attr_accessor :account, :name
     def initialize(account, name)
-      self.account = account
-      self.name = name
+      @account = account
+      @name = name
     end
 end
