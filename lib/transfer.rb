cf346ab80d440fa991d6f679a637d56866f25d23
Eugene O'Neil <ejo7220@gmail.com>
2020-08-11 17:14:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,10 @@
 class Transfer
-  
-  def initialize()
+  attr_accessor :sender, :reciever, :amount, :account
+  def initialize(sender, reciever, amount)
+    @sender = sender
+    @reciever = reciever 
+    @amount = amount
+    @status = "pending"
     
   end 
   
