db24092363defcecf5d0a21b8012e58bfcbdd76c
Eugene O'Neil <ejo7220@gmail.com>
2020-08-11 17:40:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,8 +23,8 @@ class Transfer
       elsif @status == "complete"
       puts "Transaction was already executed."
     else
-      sender.deposit(@amount * -1)
-      receiver.deposit(@amount)
+      @sender.deposit(@amount * -1)
+      @receiver.deposit(@amount)
       @status = "complete"
       
       
