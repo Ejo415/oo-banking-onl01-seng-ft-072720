3d5977952e60f110c78daa89d50d712193ddd936
Eugene O'Neil <ejo7220@gmail.com>
2020-08-11 17:48:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,17 +16,16 @@ class Transfer
     end
   end
   
-  def execute_transaction 
-    if @sender.balance < @amount 
+  def execute_transaction
+    if @sender.balance < @amount
       @status = "rejected"
-     puts "Transaction rejected. Please check your account balance."
-     
-      elsif @status == "complete"
-      puts "Transaction was already executed."
-   
+      return "Transaction rejected. Please check your account balance."
+
+    elsif @status == "complete"
+      puts "Transaction was already excuted"
     else
-      @sender.deposit(@amount * -1)
-      @receiver.deposit(@amount)
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
       @status = "complete"
     end
   end
